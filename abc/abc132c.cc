f51f120369a19a1dc47d7863edf5f2a19fb2652f
jojonki <junki.ohmura@gmail.com>
2019-08-17 00:07:12 +0900

    modify

--- abc/abc132c.cc
+++ abc/abc132c.cc
@@ -2,23 +2,22 @@
 #include <vector>
 using namespace std;
 
-int main()
-{
+int main() {
     int N;
     vector<int> D;
 
     cin >> N;
-    while(N--) {
+    while (N--) {
         int n;
         cin >> n;
         D.push_back(n);
     }
 
     int ct = 0;
-    if(D.size() % 2 == 0) {
+    if (D.size() % 2 == 0) {
         sort(D.begin(), D.end());
         int mid = D.size() / 2;
-        for (int i=D[mid-1]+1; i<=D[mid]; i++) ct++;
+        for (int i = D[mid - 1] + 1; i <= D[mid]; i++) ct++;
     }
 
     cout << ct << endl;
