a11073780e4b511409f48a60ff61a666c8b01fd2
jojonki <junki.ohmura@gmail.com>
2019-09-01 20:38:40 +0900

    update template

--- abc/code_template.cc
+++ abc/code_template.cc
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <iomanip>
+#define REP(i, n) for(int i = 0; i < n; i++)
+template<class T> inline void chmin(T& a, T b) {if (a>b) a=b; }
+template<class T> inline void chmax(T& a, T b) {if (a<b) a=b; }
 using namespace std;
+typedef long long ll;
+const ll LINF = 1e18;
+const int INF = 1e9;
 
 int main() { 
     
