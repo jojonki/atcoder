036b31f251c064f0840f12f673a45c68a5e2b75f
jojonki <junki.ohmura@gmail.com>
2019-09-08 10:24:53 +0900

    move test files

--- abc/gen_template.sh
+++ abc/gen_template.sh
@@ -8,7 +8,9 @@ fi
 cp code_template.cc ./abc$1a.cc
 cp code_template.cc ./abc$1b.cc
 cp code_template.cc ./abc$1c.cc
+cp code_template.cc ./abc$1d.cc
 
-touch ./t_abc$1a.txt
-touch ./t_abc$1b.txt
-touch ./t_abc$1c.txt
+touch ./t/t_abc$1a.txt
+touch ./t/t_abc$1b.txt
+touch ./t/t_abc$1c.txt
+touch ./t/t_abc$1d.txt
