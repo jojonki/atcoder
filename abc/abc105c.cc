9086763af695de639c82eec8e140ab894da316c9
jojonki <junki.ohmura@gmail.com>
2019-10-06 09:57:37 +0900

    abc105c

---
+++ abc/abc105c.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+template<class T> inline void chmin(T& a, T b) {if (a>b) a=b; }
+template<class T> inline void chmax(T& a, T b) {if (a<b) a=b; }
+inline void print() { cout << endl; }
+template <class Head, class... Tail> inline void print(Head&& head, Tail&&... tail) {cout << head; if (sizeof...(tail) != 0) cout << " "; print(forward<Tail>(tail)...);}
+template <class T> inline void print(vector<T>& vec) { for (auto& a : vec) {cout << a; if (&a != &vec.back()) cout << " "; } cout << endl;}
+template <class T> inline void print(vector<vector<T>>& df) { for (auto& vec : df) { print(vec); }}
+typedef long long ll;
+const ll LINF = 1e18;
+const int INF = 1e9;
+
+
+int main() {
+    int N; cin >> N;
+    if(N==0) {
+        print(0);
+        return 0;
+    }
+    string s = "";
+    while(N!=0) {
+        int r = (N%(-2));
+        if(r < 0) r += 2;
+
+        N = (N-r) / (-2);
+        s += char('0' + r);
+    }
+
+    reverse(s.begin(), s.end());
+
+    print(s);
+}
