6b7030ecd94959ad4e5e34ab12b288ed904d7d87
jojonki <junki.ohmura@gmail.com>
2020-01-26 10:23:26 +0900

    change template

--- abc/gen_template.sh
+++ abc/gen_template.sh
@@ -15,6 +15,7 @@ fi
 # python
 for char in `echo $2 | fold -w 1`; do
   echo "Generate abc$1$char" 
-  touch ./abc$1$char.py
+  # touch ./abc$1$char.py
+  cp template.py ./abc$1$char.py
   touch ./t/t_abc$1$char.txt
 done
