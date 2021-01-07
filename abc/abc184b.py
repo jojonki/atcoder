bf89e54e98df6ff6751a55484699df7369ac867d
jojonki <junki.ohmura@gmail.com>
2021-01-07 21:28:28 +0900

    add abc 184 abc

---
+++ abc/abc184b.py
@@ -0,0 +1,12 @@
+def main():
+    N, X = map(int, input().split())
+    S = input()
+    for c in S:
+        if c == 'o':
+            X += 1
+        else:
+            X = max(0, X - 1)
+    print(X)
+
+
+main()
---
+++ abc/abc184c.py
@@ -0,0 +1,15 @@
+def main():
+    r1, c1 = map(int, input().split())
+    r2, c2 = map(int, input().split())
+    p, q = abs(r2 - r1), abs(c2 - c1)
+    if p == q == 0:
+        print(0)
+    elif p == 1 or p + q <= 3:
+        print(1)
+    elif (p + q) % 2 == 0 or p + q <= 6 or abs(p - q) <= 3:
+        print(2)
+    else:
+        print(3)
+
+
+main()
