88c2ee9ec977d2e91cc083c07f5f8075206d6b50
jojonki <junki.ohmura@gmail.com>
2021-01-16 10:42:18 +0900

    add 173d

--- abc/abc173d.py
+++ abc/abc173d.py
@@ -1,4 +1,11 @@
 def main():
-    
+    N = int(input())
+    A = list(map(int, input().split()))
+    A.sort(reverse=True)
+    ans = A[0]
+    for i in range(2, N):
+        ans += A[i // 2]
+    print(ans)
+
 
 main()
