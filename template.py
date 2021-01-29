8c3abe634120cae513ffd37811e80547f92bfbf8
jojonki <junki.ohmura@gmail.com>
2021-01-29 17:30:34 +0900

    mv template

---
+++ template.py
@@ -0,0 +1,18 @@
+import sys
+sys.setrecursionlimit(1 << 20)
+INF = float('inf')
+
+
+def read_int_list():
+    return list(map(int, input().split()))
+
+
+def read_ints():
+    return map(int, input().split())
+
+
+def main():
+    
+
+
+main()
