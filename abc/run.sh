5f13d0ac25051779b5e0a4b1f7b12a0fd0a751c4
jojonki <junki.ohmura@gmail.com>
2021-01-29 16:12:42 +0900

    use pypy3

--- abc/run.sh
+++ abc/run.sh
@@ -5,4 +5,5 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
-~/anaconda3/envs/atcoder/bin/python abc$1$2.py < ./t/t_abc$1$2.txt
+#~/anaconda3/envs/atcoder/bin/python abc$1$2.py < ./t/t_abc$1$2.txt
+/usr/local/bin/pypy3 dp_$1.py < ./t/t_dp_$1.txt
